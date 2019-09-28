727a747f8698a1da9efd27f1f06ad04bb39b118a
cedarit <pankaj01@gmail.com>
2019-09-28 15:54:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# af-ecom-1end-rmosyv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/af-ecom-1end-rmosyv)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'iPhone 11 Pro',
+    price: 70000,
+    description: 'Pro cameras. Pro display. Pro performance.'
+  },
+  {
+    name: 'iPhone 11',
+    price: 699,
+    description: 'Just the right amount of everything.'
+  },
+  {
+    name: 'iPhone X',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
